e52564e07ad0712c0f7d3010884b5961bc788da3
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-10-27 11:54:57 +0200

    initial commit

---
+++ src/rational/Rational.java
@@ -0,0 +1,10 @@
+package rational;
+/**
+ * Represents a Rational number.
+ * Inspired by Eric S. Roberts: The Art and Science of Java.
+ * https://cs.stanford.edu/people/eroberts/books/TheArtAndScienceOfJava/index.html
+ *
+ * @author Barne Kleinen
+ */
+public class Rational {
+}
