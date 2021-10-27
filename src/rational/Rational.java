548fe12e74e72136d402ee3bd94defe9da2438e5
Barne Kleinen <barne.kleinen@htw-berlin.de>
2021-10-27 16:33:23 +0200

    add sicp link

--- src/rational/Rational.java
+++ src/rational/Rational.java
@@ -3,7 +3,7 @@ package rational;
  * Represents a Rational number.
  * Inspired by Eric S. Roberts: The Art and Science of Java.
  * https://cs.stanford.edu/people/eroberts/books/TheArtAndScienceOfJava/index.html
- *
+ * https://mitpress.mit.edu/sites/default/files/sicp/full-text/book/book-Z-H-14.html#%_sec_2.1.1
  * @author Barne Kleinen
  */
 public class Rational {
